23ab568f6b2d7966bf619a2fcbd6c07a834e8290
Luis Enrique Jimenez Bernal <luisenriquejimenezber@gmail.comgit config --global --list user.email luisenriquejimenezber@gmail.com>
2026-09-22 21:12:58 -0600

    Actualiza archivos

---
+++ Calculadora-JS/Calculadora-JS.js
@@ -0,0 +1,16 @@
+// Elaboracion de calculadora, funciona solo en console de navegador
+let nombre = prompt("Como te llamas?");
+let numero1 = Number(prompt("Escribe el primer número"));
+let numero2 = Number(prompt("Escribe el segundo número"));
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+    console.log("Suma: " + suma);   
+    console.log("Resta: " + resta);
+    console.log("Multiplicación: " + multiplicacion);
+    console.log("División: " + division);
+    console.log("Hola" + nombre + ", estos son tus resultados:");
+    
