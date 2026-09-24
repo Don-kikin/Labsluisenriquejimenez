9574c29c249b135ecfacec1e5db57c39b69568bb
Luis Enrique Jimenez Bernal <luisenriquejimenezber@gmail.comgit config --global --list user.email luisenriquejimenezber@gmail.com>
2026-09-24 15:06:15 -0600

    Actualizacion de GItHUB archivos

---
+++ LAB7 SD-06 TIFC2DEV-SD-06 - Lab_ Construcción de Objetos_Intsrucciones para Participantes (1)/task3.js
@@ -0,0 +1,13 @@
+// Type your code below this line!
+
+function Mail(subj, msg) {
+    this.subject = subj
+    this.message = msg
+    
+  }
+  
+  const newMail = new Mail("pizza", "pineapple" )
+  
+  // Type your code above this line!
+  
+  newMail.printMail()
