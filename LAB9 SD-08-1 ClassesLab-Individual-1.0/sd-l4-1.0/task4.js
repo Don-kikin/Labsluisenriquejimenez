645b6f7042d7c69791e36da19013f9eee2eb637d
Luis Enrique Jimenez Bernal <luisenriquejimenezber@gmail.comgit config --global --list user.email luisenriquejimenezber@gmail.com>
2026-09-25 17:36:11 -0600

    Actualizacion de GItHUB archivos

--- LAB9 SD-08-1 ClassesLab-Individual-1.0/sd-l4-1.0/task4.js
+++ LAB9 SD-08-1 ClassesLab-Individual-1.0/sd-l4-1.0/task4.js
@@ -2,13 +2,12 @@ export class Player {
   constructor(player, level) {
     this.name = player;
     this.level = level;
-    this.levelup = level + 1;
   }
   
-  Info() {
+  info() {
     return this.name + ' has reached Level ' + this.level + '!';
   }
-  Info
-    return this.name + ' has reached Level ' + this.levelup + '!';
+  levelUp(){
+    this.level += 1;
   }
 }
--- LAB9 SD-08-1 ClassesLab-Individual-1.0/sd-l4-1.0/task5.js
+++ LAB9 SD-08-1 ClassesLab-Individual-1.0/sd-l4-1.0/task5.js
@@ -1,7 +1,20 @@
 export class Player {
-    constructor() {
-      
-    }
-  
-    
+  constructor(player, level) {
+    this.name = player;
+    this.level = level;
+    this.xp = 0;
+    this.xpRequired = 100; // Experiencia necesaria para el siguiente nivel
   }
+
+  info() {
+    return this.name + ' has reached Level ' + this.level + '!';
+  }
+
+  gainXp(points) {
+    this.xp += points;
+    while (this.xp >= this.xpRequired) {
+      this.xp -= this.xpRequired; 
+      this.level += 1;
+    }
+  }
+}
